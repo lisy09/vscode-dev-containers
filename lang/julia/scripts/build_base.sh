5ad90b5c043f48df2ff81ca60b0d58792ebc1218
lisy09 <lisy09.thu@gmail.com>
2021-02-21 22:14:33 +0900

    add julia dev container

---
+++ lang/julia/scripts/build_base.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly ROOT_DIR="$( cd $SCRIPT_DIR/.. >/dev/null 2>&1 && pwd )"
+source $ROOT_DIR/.env
+
+COMMANDS=docker
+IFS=',' read -a commands <<< ${COMMANDS}
+for COMMAND in ${commands[@]}; do
+    if ! command -v ${COMMAND} &> /dev/null; then
+        echo "Command could not be found: ${COMMAND}"
+        exit 1
+    fi
+done
+
+set -e
+set -x
+
+docker build \
+    --build-arg GOLANG_VERSION=${GOLANG_VERSION} \
+    -t ${BASE_IMAGE_FULL} \
+    ${ROOT_DIR}/docker
---
+++ lang/julia/src/main.jl
@@ -0,0 +1,1 @@
+println("Hello Remote World!")
